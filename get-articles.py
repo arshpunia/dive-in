b205cf9bc08dd35bc3e1e53353833430b24d9b33
arshpunia <arshp98@my.yorku.ca>
2020-06-23 07:58:27 -0400

    Skeletal functionality. Need to clean code and document it

--- get-articles.py
+++ get-articles.py
@@ -5,6 +5,8 @@ from urllib.request import Request,urlopen
 import logging 
 import os
 from dotenv import load_dotenv
+import webbrowser as wb
+import random
 
 def get_request_token():
     load_dotenv()
@@ -25,7 +27,13 @@ def get_access_token():
     json = urlopen(request).read().decode()
     print(json)
 
+def register_browser():
+    chrome_path="C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+    wb.register('chrome', None,wb.BackgroundBrowser(chrome_path))
+
+
 def make_get_requests():
+    register_browser()
     load_dotenv()
     consumer_key = os.getenv('POCKET_API_KEY')
     access_token = os.getenv('POCKET_ACCESS_TOKEN')
@@ -34,10 +42,21 @@ def make_get_requests():
     json_rep = response.json()
     list = json_rep['list']
     count=0
+    
     for header in list:
         count+=1
-        print(list[str(header)]['resolved_title'])
-    print(str(count))
+ 
+        ##print(list[str(header)]['resolved_title'])
+    article_num = random.randint(0,count-1) 
+    a_count=0
+    print(str(article_num))
+    for header in list:
+        a_count+=1
+        if a_count==article_num:
+            wb.get('chrome').open(list[str(header)]['given_url'])
+            break
+        ##print(list[str(header)]['resolved_title'])
+    print(str(a_count))
     
 def main():
     make_get_requests()
